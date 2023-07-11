6c7526910fd2351b47e8dfbbdf6985a94c2fc9e8
SUBHArthy <maitysubharthy@gmail.com>
2023-07-11 14:15:57 +0530

    //STARTER

---
+++ Z1Z1Z1/src/test/java/sample/Parameters.java
@@ -0,0 +1,29 @@
+package sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Parameters {
+
+	@Test
+	public void m1() {
+
+		String BROWSER = System.getProperty("browser");
+		String url = System.getProperty("url");
+
+		System.out.println(BROWSER);
+		System.out.println(url);
+
+		WebDriver driver = null ;
+		if (BROWSER.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+
+		driver.get(url);
+
+	}
+}
